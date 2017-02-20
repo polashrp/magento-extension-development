68e3b6cf390d5bd472ccc8af3d86d69bc2e74044
habib.ditp@gmail.com <habib.ditp@gmail.com>
2017-02-20 18:06:11 +0600

    first commit

---
+++ Block/DitpModule.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+* 
+*/
+
+namespace Mageplaza\DitpModule\Block;
+
+class DitpModule extends \Magento\Framework\View\Element\Template
+{
+    public function getHelloWorldTxt()
+    {
+        return 'This is My First Magento Extension.';
+    }
+}
